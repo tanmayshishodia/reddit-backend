1ce7b1d1c4364e7d5d5c44b48188fbf136bc8ec3
tanmayshishodia <tanmay@Tanmay-Lappy.localdomain>
2021-03-10 12:29:16 +0530

    configured post model

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+//User POST
+const mongoose = require('mongoose')
+const uploadPostSchema = new mongoose.Schema({
+    uid : {
+        type: String,
+        unique: false,
+        required: true
+    },
+    caption : {
+        type: String,
+        unique: false,
+        required: true
+    },
+    desc : {
+        type: String,
+        unique: false,
+        required: false
+    },
+    filename : {
+        type: String,
+        unique: true,
+        required: false
+    },
+    contentType : {
+        type: String,
+        required: false
+    },
+    imageBase64 : {
+        type: String,
+        required: false
+    },
+    votes : {             
+        type: Number,
+        required: true,
+    }
+  })
+  
+  //use uploadPostModel to post.
+  module.exports = uploadPostModel = mongoose.model('Post_uploads', uploadPostSchema);
