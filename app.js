0fa0a6e75a0c99e9eda0cbe12c74527a0aa60640
roy-a2yush <roy.a2yush@gmail.com>
2021-03-15 10:05:29 +0530

    Added cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo').default
 const connectDB = require('./config/db')
 const AWS = require('aws-sdk')
+const cors = require('cors')
 //const uuid = require('uuid/v4')
 const { v4: uuid } = require('uuid');
 
@@ -22,10 +23,28 @@ connectDB()
 
 const app = express()
 
+
+//Newly added -----------------------
+let whitelist = ['http://localhost:8000', 'https://accounts.google.com/o/oauth2/v2/auth']
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) === -1) {
+      var message = 'The CORS policy for this origin doesn ' +
+        'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+//end -------------------------------
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-  }
+  app.use(morgan('dev'))
+}
 
 // Sessions
 app.use(
