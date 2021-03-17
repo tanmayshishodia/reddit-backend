544c333470923f2ef79de4cac5c012f7a01cc949
roy-a2yush <roy.a2yush@gmail.com>
2021-03-17 18:25:41 +0530

    authorize

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -6,7 +6,7 @@ const User = require('../models/User')
 module.exports = () => {
     return (req, res, next) => {
         var uid1 = req.headers['uid']
-        if(uid1 == "null" || !uid1 || uid1 == "\"\"" || uid1 == undefined || uid1 == null || uid1.length <10) {
+        if(uid1 == undefined || uid1 == null || uid1 == "null" || uid1 == "\"\"" || uid1.length != 26 || !uid1) {
             console.log("First line of defence")
             return res.status(401).send("unauthorized Access")
         } else {
@@ -23,7 +23,7 @@ module.exports = () => {
                         console.log("Result : ", docs);
                         if(!docs) {
                             console.log("docs empty")
-                            res.status(401).send("err")
+                            res.status(401).send("unauthorized access")
                         } else {
                             next()
                         }
