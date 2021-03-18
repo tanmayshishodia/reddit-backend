7edb570dca12f98a2916ae0bef850ab7f4bec13a
Tanmay Shishodia <tanmayshishodia@gmail.com>
2021-03-18 22:10:58 +0530

    configured unit test cases

--- test/post.test.js
+++ test/post.test.js
@@ -11,6 +11,14 @@ dotenv.config({
 
 connectDB()
 
+describe("GET /post/:id", () => {
+    test("It responds with single post in json format", async (done) => {
+        const response = await request(app).get("/post/604fa778019cb9437f82e8ef").set('uid', '"604fa60313489641f90db5ad"');
+        expect(response.statusCode).toBe(200);
+        done()
+    }, 30000);
+});
+
 describe("GET /poststate", () => {
     test("It responds with state of posts for different users in json format", async (done) => {
         const response = await request(app).get("/poststate").set('uid', '"604fa60313489641f90db5ad"');
@@ -19,5 +27,17 @@ describe("GET /poststate", () => {
         expect(response.body[0]).toHaveProperty("state");
         expect(response.statusCode).toBe(200);
         done()
-    });
+    }, 30000);
 });
+
+describe("POST /upload", () => {
+    test("Create a post by an authenticated user", async () => {
+      const upload = await request(app)
+        .post("/upload")
+        .set('uid', '"604fa60313489641f90db5ad"')
+        .send({
+            "caption": "test"
+        });
+      expect(upload.statusCode).toBe(200);
+    }, 30000);
+  });
