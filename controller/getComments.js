0c4ba746e382e31f83f67628696ee7d29f7f3364
roy-a2yush <roy.a2yush@gmail.com>
2021-03-18 09:29:10 +0530

    Comments + votes

--- controller/getComments.js
+++ controller/getComments.js
@@ -1,5 +1,33 @@
 let Comment = require('../models/Comment')
+let Post = require('../models/Post')
+let CommentState = require('../models/CommentState')
 let User = require('../models/User')
+const mongoose = require('mongoose')
+
+
+let creatorId
+function findCreatorId(id) {
+
+    return new Promise(async (resolve, reject) => {
+
+        const result = await CommentState.find({ uid: id }, function (err, docs) {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err)
+                reject(err)
+            }
+            else {
+                console.log("docs---- : ", docs);
+                creatorId = docs
+                console.log("creatorId: ", creatorId)
+                resolve()
+            }
+        });
+    })
+}
+
+
+
 exports.getAllComments = function (req, res) {
     let sort;
     action = req.query.action
@@ -21,34 +49,18 @@ exports.getAllComments = function (req, res) {
             break;
         default:
             sort = {
-                votes: -1
+                createdAt: -1
             }
     }
 
-    //var uid1 = req.headers.uid
-    //uid1 = mongoose.Types.ObjectId(uid1.substring(1, uid1.length - 1));
-
-    Comment.find({ postId: req.params.id, parentId: null }).populate({
-        path: 'uid'
-    }).sort(sort).exec(function (err, doc) {
-        if (err) throw err;
-        if (doc.length) {
-            res.send(doc)
-        } else {
-            res.status(404);
-            res.send({
-                error: `Unable to find posts.`
-            })
-        }
-
-        /*Comment.aggregate([{
-            $lookup: {
-                from: "users",
-                localField: "uid",
-                foreignField: "_id",
-                as: "test"
-            }
-        }]).sort(sort).exec(function (err, doc) {
+    var uid1 = req.headers.uid
+    console.log("UID:---------> ", uid1)
+    if (uid1 == undefined)
+        console.log("UNDEFINED")
+    if (uid1 == "null" || uid1 == "\"\"" || uid1 == undefined) {
+        Comment.find({ postId: req.params.id, parentId: null }).populate({
+            path: 'uid'
+        }).sort(sort).exec(function (err, doc) {
             if (err) throw err;
             if (doc.length) {
                 res.send(doc)
@@ -58,8 +70,40 @@ exports.getAllComments = function (req, res) {
                     error: `Unable to find posts.`
                 })
             }
-        })*/
-    })
+        })
+    } else {
+        try {
+            uid1 = mongoose.Types.ObjectId(uid1.substring(1, uid1.length - 1));
+            console.log("uid:> ", uid1)
+            imgLoc = findCreatorId(uid1).then(async () => {
+                console.log("--------", creatorId, "----------")
+                let map = {}
+                creatorId.forEach(element => {
+                    map[element.commentId] = element.state
+                    //console.log("key: ", element.postId, "value: ", element.state)
+                });
+                console.log(map)
+                Comment.find({ postId: req.params.id, parentId: null }).populate({
+                    path: 'uid'
+                }).sort(sort).exec(function (err, doc) {
+                    if (err) throw err;
+                    if (doc.length) {
+                        console.log("doc: ", doc, ": cod")
+                        let map1 = doc.concat(map)
+                        console.log("000000000000001111111111111222222223454675--->", map)
+                        res.send(map1)
+                    } else {
+                        res.status(404);
+                        res.send({
+                            error: `Unable to find posts.`
+                        })
+                    }
+                })
+            })
+        } catch(err) {
+            res.send(401).send("unauthorized access")
+        }
+    }
 }
 
 
