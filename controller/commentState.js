6177ef832c2394004954683b4828a29902a4982a
roy-a2yush <roy.a2yush@gmail.com>
2021-03-19 13:32:22 +0530

    resolved set res after send

--- controller/commentState.js
+++ controller/commentState.js
@@ -15,14 +15,10 @@ function findCreatorId(id) {
     return new Promise((resolve, reject) => {
         Comment.findById(id, async (err, docs) => {
             if (err) {
-                //console.log(err);
-                res.status(500).send(err)
+                res.status(400).send(err)
                 reject(err)
             }
             else {
-                //console.log("Result---- : ", docs);
-                //creatorId = docs.uid
-                //console.log("creatorid------: ", creatorId)
                 creatorId = docs.uid
                 resolve()
             }
@@ -32,185 +28,122 @@ function findCreatorId(id) {
 
 exports.commentState = async (req, res, next) => {
 
-    //check if already votes
-    //console.log(req.body)           //empty IDK why
-    //console.log(req.headers.uid)
     var uid1 = req.headers.uid
     uid1 = mongoose.Types.ObjectId(uid1.substring(1, uid1.length - 1));
 
     const result = await CommentState.find({ uid: uid1, commentId: req.params.id }, async (err, docs) => {
         if (err) {
             res.status(400).send("Something went wrong")
-            //console.log(err);
         } else {
 
             imgLoc = findCreatorId(req.params.id).then(async () => {
-                //updateKarma.updateKarma(creatorId, req, "increment", 2)
-                //res.send("Done")
-            
-            //finding id of the post creater
-            // let postId, creatorId
-            // console.log("CommentId; ",req.params.id)
-            // Comment.findById(req.params.id, function (err, docs) { 
-            //     if (err){ 
-            //         console.log(err); 
-            //         res.send(error)
-            //     } 
-            //     else{ 
-            //         console.log("Result : ", docs); 
-            //         postId = docs.postId
-            //         console.log("##########!@@@@@@@@@@@@@@@@@!**************")
-            //         console.log(postId)
-            //     } 
-            // });
-            // Post.findById(postId, function (err, docs) { 
-            //     if (err){ 
-            //         console.log(err); 
-            //         res.send(error)
-            //     } 
-            //     else{ 
-            //         console.log("Result : ", docs); 
-            //         creatorId = docs.uid
-            //         console.log("##########!@@@@@@@@@@@@@@@@@!**************")
-            //         console.log(creatorId)
-            //     } 
-            // });
 
-            //console.log(docs)
-            //console.log(docs[0].state)
-            //console.log("action: ", req.body.actions)
-            if (Object.keys(docs).length == 0) {
-                //add 1-0
+                if (Object.keys(docs).length == 0) {
+                    //add 1-0
 
-                let voteState = -1
-                //console.log("Decrement")
-                if(req.body.actions == "increment") {
-                    voteState = 1
-                    //console.log("-------Changed-------")
-                }
-
-                post = {
-                    uid: uid1,
-                    commentId: req.params.id,
-                    state: voteState
-                }
-
-                //console.log("-----------")
-                //console.log(post)
-
-                let newUpload = new CommentState(post);
-
-                try {
-                    const a1 = await newUpload.save()
-
-                    //calling updateKarma
-                    
-
-                    updateKarma.updateKarma(creatorId, req, req.body.actions, 1)
-                    Comment.updateOne({
-                        _id: mongoose.Types.ObjectId(req.params.id)
-                    }, {
-                        $inc: {
-                            votes: voteState
-                        }
-                    }, function (err, result) {
-                        if (err) throw err;
-    
-                        //console.log(`[${req.params.id}] post edited!`)
-                        //res.redirect("/")
-                        //updateKarma.updateKarma(creatorId, req, req.body.actions, -1)
-                        //
-                        //console.log(voteState)
-                        res.send("upvoted")
-                    })
-
-                    res.json(a1)
-                } catch (err) {
-                    //console.log(err)
-                    res.status(401).send(err)
-                }
-
-            } else if (docs[0].state == 1 && req.body.actions == "increment" || docs[0].state == -1 && req.body.actions == "decrement") {
-                //console.log("----SAME VOTE----")
-                //update to make 0
-                CommentState.updateOne({
-                    uid: uid1,
-                    commentId: req.params.id
-                }, {
-                    state: 0
-                }, function (err, result) {
-                    if (err) throw err;
-
-                    //console.log(`[${req.params.id}] post edited!`)
-                    //res.redirect("/")
-                    updateKarma.updateKarma(creatorId, req, req.body.actions, -1)
-                    var increase = -1
-                    if(req.body.actions == "decrement")
-                        increase = 1
-                    Comment.updateOne({
-                        _id: mongoose.Types.ObjectId(req.params.id)
-                    }, {
-                        $inc: {
-                            votes: increase
-                        }
-                    }, function (err, result) {
-                        if (err) throw err;
-    
-                        //console.log(`[${req.params.id}] post edited!`)
-                        //res.redirect("/")
-                        //updateKarma.updateKarma(creatorId, req, req.body.actions, -1)
-                        //res.json(a1)
-                        //console.log(increase)
-                        res.send("upvoted")
-                    })
-                })
-                //res.send("Done")
-            } else {
-                //console.log("-----DIFF VOTE-----")
-                //update to make -1
-                let voteState = -1
-                if(req.body.actions == "increment")
-                    voteState = 1
-
-                CommentState.updateOne({
-                    uid: uid1,
-                    commentId: req.params.id
-                }, {
-                    state: voteState
-                }, function (err, result) {
-                    if (err) throw err;
-
-                    //console.log(`[${req.params.id}] post edited!`)
-                    var increment = 1 * voteState
-                    if(voteState == 1 && docs[0].state == -1 || voteState == -1 && docs[0].state == 1) {
-                        updateKarma.updateKarma(creatorId, req, req.body.actions, 2)
-                        increment = 2 * voteState
-                    } else {
-                    updateKarma.updateKarma(creatorId, req, req.body.actions, 1)
+                    let voteState = -1
+                    if (req.body.actions == "increment") {
+                        voteState = 1
                     }
 
-                    Comment.updateOne({
-                        _id: mongoose.Types.ObjectId(req.params.id)
-                    }, {
-                        $inc: {
-                            votes: increment
-                        }
-                    }, function (err, result) {
-                        if (err) throw err;
-    
-                        //console.log(`[${req.params.id}] post edited!`)
-                        //res.redirect("/")
-                        //updateKarma.updateKarma(creatorId, req, req.body.actions, -1)
+                    post = {
+                        uid: uid1,
+                        commentId: req.params.id,
+                        state: voteState
+                    }
+
+                    let newUpload = new CommentState(post);
+
+                    try {
+                        const a1 = await newUpload.save()
+
+                        updateKarma.updateKarma(creatorId, req, req.body.actions, 1)
+                        Comment.updateOne({
+                            _id: mongoose.Types.ObjectId(req.params.id)
+                        }, {
+                            $inc: {
+                                votes: voteState
+                            }
+                        }, function (err, result) {
+                            if (err)
+                                res.status(400).send("Something went wrong")
+                            else
+                                res.status(200).send("upvoted") //ERRRR HERE
+                        })
+
                         //res.json(a1)
-                        //console.log(increment)
-                        res.send("upvoted")
+                    } catch (err) {
+                        res.status(401).send(err)
+                    }
+
+                } else if (docs[0].state == 1 && req.body.actions == "increment" || docs[0].state == -1 && req.body.actions == "decrement") {
+
+                    CommentState.updateOne({
+                        uid: uid1,
+                        commentId: req.params.id
+                    }, {
+                        state: 0
+                    }, function (err, result) {
+                        if (err)
+                            res.status(400).send("Something went wrong")
+                        else {
+
+                            updateKarma.updateKarma(creatorId, req, req.body.actions, -1)
+                            var increase = -1
+                            if (req.body.actions == "decrement")
+                                increase = 1
+                            Comment.updateOne({
+                                _id: mongoose.Types.ObjectId(req.params.id)
+                            }, {
+                                $inc: {
+                                    votes: increase
+                                }
+                            }, function (err, result) {
+                                if (err)
+                                    res.status(400).send("Something went wrong")
+                                else
+                                    res.send("upvoted")
+                            })
+                        }
                     })
-                    //updateKarma.updateKarma(creatorId, req, req.body.actions, 1)
-                    //res.redirect("/")
-                })
-                //res.send("Done")
-            }
-        })
+                } else {
+                    let voteState = -1
+                    if (req.body.actions == "increment")
+                        voteState = 1
+
+                    CommentState.updateOne({
+                        uid: uid1,
+                        commentId: req.params.id
+                    }, {
+                        state: voteState
+                    }, function (err, result) {
+                        if (err)
+                            res.status(400).send("Something went wrong")
+                        else {
+                            var increment = 1 * voteState
+                            if (voteState == 1 && docs[0].state == -1 || voteState == -1 && docs[0].state == 1) {
+                                updateKarma.updateKarma(creatorId, req, req.body.actions, 2)
+                                increment = 2 * voteState
+                            } else {
+                                updateKarma.updateKarma(creatorId, req, req.body.actions, 1)
+                            }
+
+                            Comment.updateOne({
+                                _id: mongoose.Types.ObjectId(req.params.id)
+                            }, {
+                                $inc: {
+                                    votes: increment
+                                }
+                            }, function (err, result) {
+                                if (err)
+                                    res.status(400).send("Something went wrong")
+                                else
+                                    res.send("upvoted")
+                            })
+                        }
+                    })
+                }
+            })
         }
     })
 }
